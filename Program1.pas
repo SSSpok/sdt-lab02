efa2609886891c4d487a4d2b12fbb3d2c02caa9d
Володя Волков <mr_spamer@mail.ru>
2017-06-23 03:42:29 +0300

    Nova 3

---
+++ Program1.pas
@@ -0,0 +1,12 @@
+Program Summa; 
+Const
+  A = 23;
+  B = 76;
+var
+  S,U: integer;
+begin
+  S := A + B; 
+  writeln('Сумма чисел 23 и 76 равна: S = ', S);
+  U :=A * B;
+  writeln('Умножение чисел 23 и 76 равна: S = ', S);
+end.
