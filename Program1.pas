d277e0d127bddfacde602f58c1972a3a4e9d9048
Володя Волков <mr_spamer@mail.ru>
2017-06-23 04:03:28 +0300

    Nova 7

--- Program1.pas
+++ Program1.pas
@@ -3,7 +3,7 @@ Const
   A = 23;
   B = 76;
 var
-  S,U: integer;
+  S,U,M: integer;
   D:real;
 begin
   S := A + B; 
@@ -12,5 +12,7 @@ begin
   writeln('Умножение чисел 23 и 76 равна: S = ', U);
   D :=A/B;
   writeln('Деление чисел 23 и 76 равна: S = ', D);
+  M :=A-B;
+  writeln('Деление чисел 23 и 76 равна: S = ', M);
  
 end.
